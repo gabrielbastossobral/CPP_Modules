0980389868fd982ed7067ca948391698f9151ce8
Gabriel Sobral <gabastos@student.42sp.org.br>
2025-07-29 09:18:02 -0300

    put test using addNumber

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -24,16 +24,27 @@ int main()
     }
 
     std::cout << "\n[ Test 3: Large scale test with 100,000 numbers ]" << std::endl;
-    const unsigned int largeSize = 100000;
-    Span bigSpan = Span(largeSize);
-
-    std::srand(std::time(0));
-    for (unsigned int i = 0; i < largeSize; ++i) {
-        bigSpan.addNumber(std::rand());
+    Span bigSpan = Span(100000);
+    
+    std::vector<int> numbers;
+    numbers.reserve(100000);
+    
+    std::srand(std::time(NULL));
+    for (int i = 0; i < 100000; ++i) {
+        numbers.push_back(std::rand() % 1000000);
     }
-
-    std::cout << "Shortest span (large): " << bigSpan.shortestSpan() << std::endl;
-    std::cout << "Longest span (large): " << bigSpan.longestSpan() << std::endl;
+    
+    std::cout << "Adding 100,000 random numbers using addNumbers()..." << std::endl;
+    
+    try {
+        bigSpan.addNumber(numbers.begin(), numbers.end());
+        std::cout << "Successfully added " << numbers.size() << " numbers!" << std::endl;
+        
+        std::cout << "Shortest span (large): " << bigSpan.shortestSpan() << std::endl;
+        std::cout << "Longest span (large): " << bigSpan.longestSpan() << std::endl;
+    } catch (std::exception &e) {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
 
     std::cout << "\n[ Test 4: Over capacity exception ]" << std::endl;
     Span smallSpan = Span(3);
